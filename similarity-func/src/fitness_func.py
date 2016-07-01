58102b2f92e3324869bb9fae80f674cb55ce4c98
Vektor Dewanto <vektor.dewanto@gmail.com>
2016-07-01 13:40:32 +0700

    fix zerodiv err np.isinf(x) -- fixed

--- similarity-func/src/fitness_func.py
+++ similarity-func/src/fitness_func.py
@@ -53,7 +53,7 @@ def getZeroDivFitness(individualStr):
     np.seterr(invalid='ignore')
     try:
         r = eval(individualStr)
-        if np.isnan(r) or np.isinf(x):
+        if np.isnan(r) or np.isinf(r):
             zeroDivFitness = 0.0
     except ZeroDivisionError as err:
         zeroDivFitness = 0.0
