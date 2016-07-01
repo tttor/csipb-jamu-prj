9d8526e9caa87d76bdccbbb4075a6387c916d7d0
Vektor Dewanto <vektor.dewanto@gmail.com>
2016-07-01 09:29:48 +0700

    ch setting and running settings

--- similarity-func/src/run.sh
+++ similarity-func/src/run.sh
@@ -11,11 +11,6 @@ EXE=main.py
 if [ $1 -eq 0 ]
 then
   	python $EXE
-elif [ $1 -eq 1 ]
-then
-	python -m scoop $EXE
-	#python -m scoop --hostfile hosts $EXE  
-else
-  	echo "[FATAL] Unknown argument."
-  	exit 1
+else 
+	python -m scoop -n $1 $EXE
 fi
