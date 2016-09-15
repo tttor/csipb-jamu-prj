67a62eff112b3435fc433a984e17b2396a23ea01
Vektor Dewanto <vektor.dewanto@gmail.com>
2016-09-15 12:36:24 +0700

    init BLM class

--- drugtarget-pred/util.py
+++ drugtarget-pred/util.py
@@ -1,14 +1,22 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 def loadYamanishi(fpath):
     with open(fpath) as f:
         content = f.readlines()
 
+    drugList = []
+    proteinList = []
     for c in content:
-        tmp = c.split()
+        tmp = [i.strip() for i in c.split()]
+        proteinList.append(tmp[0])
+        drugList.append(tmp[1])
+
+    drugSet = set(drugList)
+    proteinSet = set(proteinList)
         
     print(len(content))
-    print(content[1])
+    print(len(drugSet))
+    print(len(proteinSet))
 
 def main():
     loadYamanishi('/home/tor/robotics/prj/csipb-jamu-prj/dataset/yamanishi/ground-truth/bind_orfhsa_drug_e.txt')
