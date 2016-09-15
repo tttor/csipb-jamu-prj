5b73500d60a85ac9ff5af947f9f67f1fa24a9af5
Vektor Dewanto <vektor.dewanto@gmail.com>
2016-09-15 17:29:45 +0700

    set training testng data

--- drugtarget-pred/blm_core.py
+++ drugtarget-pred/blm_core.py
@@ -1,14 +1,53 @@
 '''
 BLM Framework by Yamanishi and Beakley
 '''
+
+import numpy as np
+from collections import defaultdict
+from sklearn.cross_validation import KFold
+from sklearn import svm
+
 class BLM:
-    data = []
+    dataX = []
+    dataY = []
+    nData = 0
 
     def __init__(self, fpath):
         self._load(fpath)
 
-    def run(self):
+    def eval(self):
         print('hello world')
+        # kf = KFold(self.nData, n_folds=self.nData) #equivalent to the Leave One Out strategy
+        kf = KFold(self.nData, n_folds=10) 
+
+        for trIdxList, testIdxList in kf:
+            DtestX = [self.dataX[i] for i in testIdxList]
+            DtestY = [self.dataY[i] for i in testIdxList]
+
+            DtrX = [self.dataX[i] for i in trIdxList]
+            DtrY = [self.dataY[i] for i in trIdxList]
+
+            DtrProteinX,DtrProteinY = self._makeDtrOfProtein(DtestX,DtrX,DtrY)
+
+            print( len(DtrProteinX) )
+            print( len(DtrProteinY) )
+            # clfOfProtein = svm.SVC() 
+            # clfOfProtein.fit(DtrProteinX,DtrProteinY)
+
+            # DpredProteinY = clfOfProtein.predict(DtestProteinX)
+            break
+
+    def _makeDtrOfProtein(self, DtestX, DtrX, DtrY):
+        DtrProteinX = []
+        DtrProteinY = []
+        testDrugList = [d[0] for d in DtestX]
+
+        for idx,d in enumerate(DtrX):
+            if (d[0] in testDrugList):
+                DtrProteinX.append(d)
+                DtrProteinY.append( DtrY[idx] )
+
+        return (DtrX, DtrY)
 
     def _load(self, fpath):
         content = []
@@ -17,17 +56,23 @@ class BLM:
 
         drugList = []
         proteinList = []
+        drugProteinDict = defaultdict(list)
         for c in content:
             tmp = [i.strip() for i in c.split()]
+            
             proteinList.append(tmp[0])
             drugList.append(tmp[1])
 
-        drugSet = set(drugList)
-        proteinSet = set(proteinList)
+            drugProteinDict[tmp[1]].append( tmp[0] )
 
-        data = [(i,j) for i in range(len(drugSet)) for j in range(len(proteinSet))]
+        drugList = list(set(drugList))
+        proteinList = list(set(proteinList))
+        assert(len(drugList)==len(drugProteinDict))
 
-        print(len(content))
-        print(len(drugSet))
-        print(len(proteinSet))
-        print(len(data))
+        self.dataX = [(i,j) for i in range(len(drugList)) for j in range(len(proteinList))]
+        for x in self.dataX:
+            targetProteinList = drugProteinDict[ drugList[x[0]] ]
+            self.dataY.append(int( proteinList[x[1]] in targetProteinList ))
+
+        assert(len(self.dataX)==len(self.dataY))
+        self.nData = len(self.dataX)
--- drugtarget-pred/main.py
+++ drugtarget-pred/main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 
+import time
 from blm_core import BLM
 
 def main():
     fpath = '/home/tor/robotics/prj/csipb-jamu-prj/dataset/yamanishi/ground-truth/bind_orfhsa_drug_e.txt'
     blm = BLM(fpath)
-    blm.run()
+    blm.eval()
 
 if __name__ == '__main__':
+    start_time = time.time()
     main()
+    print("--- %s seconds ---" % (time.time() - start_time))
