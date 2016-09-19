272429b49c83e9765c3ff38996342f746061485e
Vektor Dewanto <vektor.dewanto@gmail.com>
2016-09-19 16:04:54 +0700

    make dataset as argv

--- drugtarget-pred/main.py
+++ drugtarget-pred/main.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python
 
 import time
+import sys
 from blm_core import BLM
 
-def main():
-    fpath = '/home/tor/jamu/dataset/yamanishi/ground-truth/bind_orfhsa_drug_nr.txt'
-    proteinKernelFpath = '/home/tor/jamu/dataset/yamanishi/similarity-mat/protein/nr_simmat_dg.txt'
-    drugKernelFpath = '/home/tor/jamu/dataset/yamanishi/similarity-mat/compound/nr_simmat_dc.txt'
-    outDir = '/home/tor/jamu/xprmt/drugtarget-pred'
+def main(argv):
+    interactionFpath = argv[1]
+    drugKernelFpath = argv[2]
+    proteinKernelFpath = argv[3]
+    outDir = argv[4]
 
-    blm = BLM(fpath, drugKernelFpath, proteinKernelFpath)
+    blm = BLM(interactionFpath, drugKernelFpath, proteinKernelFpath)
     blm.eval(outDir)
 
 if __name__ == '__main__':
     start_time = time.time()
-    main()
+    main(sys.argv)
     print("--- %s seconds ---" % (time.time() - start_time))
--- drugtarget-pred/run.sh
+++ drugtarget-pred/run.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
   echo "[FATAL] Illegal number of parameters"
   exit 1
 fi
 
+DATASET=$3
+
+HOME=/home/tor/jamu
+DATASET_DIR=$HOME/dataset/yamanishi
+OUT_DIR=$HOME/xprmt/drugtarget-pred
 EXE=main.py
+
+BIND_FPATH=$DATASET_DIR/ground-truth/bind_orfhsa_drug_$DATASET.txt
+DRUG_KERNEL_FPATH=$DATASET_DIR/similarity-mat/compound/simmat_dc_$DATASET.txt
+PROTEIN_KERNEL_FPATH=$DATASET_DIR/similarity-mat/protein/simmat_dg_$DATASET.txt
 
 # 0: single
 # 1: parallel
@@ -15,6 +24,6 @@ else
 	for (( i=1; i <= $2; i++ ))
 	do
 		echo "!!! run $i of $2 ..."
-		python -m scoop -n $1 $EXE
+		python -m scoop -n $1 $EXE $BIND_FPATH $DRUG_KERNEL_FPATH $PROTEIN_KERNEL_FPATH $OUT_DIR
 	done
 fi
