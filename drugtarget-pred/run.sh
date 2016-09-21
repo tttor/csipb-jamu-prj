4750d98a2ac58b64a5526da72b77fd8aff7a5327
Vektor Dewanto <vektor.dewanto@gmail.com>
2016-09-21 15:50:10 +0700

    fix parallel arg

--- drugtarget-pred/run.sh
+++ drugtarget-pred/run.sh
@@ -13,21 +13,20 @@ HOME=/home/tor/jamu
 DATASET_DIR=$HOME/dataset/yamanishi
 OUT_DIR=$HOME/xprmt/drugtarget-pred/$DATASET
 
-BIND_FPATH=$DATASET_DIR/ground-truth/bind_orfhsa_drug_$DATASET.txt
+BIND_FPATH=$DATASET_DIR/ground-truth/admat_dgc_$DATASET.txt
 DRUG_KERNEL_FPATH=$DATASET_DIR/similarity-mat/compound/simmat_dc_$DATASET.txt
 PROTEIN_KERNEL_FPATH=$DATASET_DIR/similarity-mat/protein/simmat_dg_$DATASET.txt
 
 # 0: single
 # 1: parallel
-if [ $1 -eq 0 ]
-then
-  	python $EXE
-else 
-	for (( i=1; i <= $2; i++ ))
-	do
-		echo "!!! run $i of $2 ..."
-		python -m scoop -n $1 \
-		$EXE $BIND_FPATH $DRUG_KERNEL_FPATH $PROTEIN_KERNEL_FPATH \
-			 $OUT_DIR $TYPE $DATASET
-	done
+if [ $1 -ne 0 ]; then
+    FLAG="-m scoop -n $1"
 fi
+
+for (( i=1; i <= $2; i++ ))
+do
+    echo "!!! run $i of $2 ..."
+    python $FLAG \
+    $EXE $BIND_FPATH $DRUG_KERNEL_FPATH $PROTEIN_KERNEL_FPATH \
+         $OUT_DIR $TYPE $DATASET
+done
