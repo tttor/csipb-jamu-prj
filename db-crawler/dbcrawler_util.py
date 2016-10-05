9c616f973362a8a802822c64ea6e7a456c76a55a
Vektor Dewanto <vektor.dewanto@gmail.com>
2016-10-05 18:13:00 +0700

    insert compound from knapsack

--- db-crawler/dbcrawler_util.py
+++ db-crawler/dbcrawler_util.py
@@ -6,6 +6,8 @@ def mysqlCommit(db, cursor, query):
     try:
         cursor.execute(query)
         db.commit()
+
+        return cursor.fetchone()
     except (MySQLdb.Error, MySQLdb.Warning) as e:
         db.rollback()
         assert False, 'mySQL Error: '+str(e)
