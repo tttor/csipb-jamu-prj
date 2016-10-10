819651169fbc37c7c7951adf1e78391515937310
Vektor Dewanto <vektor.dewanto@gmail.com>
2016-10-10 16:41:49 +0700

    try catch open url err

--- db-crawler/crawl_kegg.py
+++ db-crawler/crawl_kegg.py
@@ -17,11 +17,11 @@ db = MySQLdb.connect("localhost","root","123","ijah" )
 cursor = db.cursor()
 
 def main(argv):
-    # lo = int(argv[1]); hi = int(argv[2])
-    # parseCompoundWebpage(lo,hi)
+    lo = int(argv[1]); hi = int(argv[2])
+    parseCompoundWebpage(lo,hi)
 
-    dirpath = '/home/tor/robotics/prj/csipb-jamu-prj/dataset/kegg/kegg_20161010x'
-    insertCompoundData(dirpath)
+    # dirpath = '/home/tor/robotics/prj/csipb-jamu-prj/dataset/kegg/kegg_20161010x'
+    # insertCompoundData(dirpath)
 
 def insertCompoundData(dirpath):
     data = {}
@@ -45,8 +45,6 @@ def insertCompoundData(dirpath):
 
         #TODO
 
-
-
 def parseCompoundWebpage(loIdx, hiIdx):
     baseFpath = '/home/tor/robotics/prj/csipb-jamu-prj/dataset/kegg/kegg_20161010/'
     baseURL = 'http://www.genome.jp/dbget-bin/www_bget?cpd:'
@@ -58,44 +56,55 @@ def parseCompoundWebpage(loIdx, hiIdx):
         msg = 'parsing i= '+ str(i+1)+ '/'+ str(n)
         idStr = 'C'+str(j).zfill(5)
 
+        # url = baseFpath+idStr+ '.html'
         url = baseURL+idStr
-        html = urllib.urlopen(url)
         msg = msg + ' from '+ url
-
-        # fpath = baseFpath+idStr+ '.html'
-        # with open(fpath, 'r') as content_file:
-        #     html = content_file.read()
-        # msg = msg + ' from '+ fpath
-
         print msg
-        soup = bs(html,'html.parser')
+
+        html = None
+        # with open(url, 'r') as content_file:
+        #     html = content_file.read()
+        try: 
+            html = urllib.urlopen(url)
+        except urllib.HTTPError, e:
+            print('HTTPError = ' + str(e.code))
+        except urllib.URLError, e:
+            print('URLError = ' + str(e.reason))
+        except httplib.HTTPException, e:
+            print('HTTPException')
+        except Exception:
+            import traceback
+            print('generic exception: ' + traceback.format_exc())
+
         datum = {}
+        if html!=None:
+            soup = bs(html,'html.parser')
+            
+            hrefDict = {}
+            hrefDict['keggDrugId'] = '/dbget-bin/www_bget?dr:'
+            hrefDict['pubchemSid'] = 'http://pubchem.ncbi.nlm.nih.gov/summary/summary.cgi?sid='
+            hrefDict['knapsackId'] = 'http://kanaya.naist.jp/knapsack_jsp/information.jsp?sname=C_ID&word='
+
+            aList = soup.find_all('a')
+            for a in aList:
+                href = str(a.get('href'))
+                for k,h in hrefDict.iteritems():
+                    if h in href:
+                        datum[k] = href.strip(h)
         
-        hrefDict = {}
-        hrefDict['keggDrugId'] = '/dbget-bin/www_bget?dr:'
-        hrefDict['pubchemSid'] = 'http://pubchem.ncbi.nlm.nih.gov/summary/summary.cgi?sid='
-        hrefDict['knapsackId'] = 'http://kanaya.naist.jp/knapsack_jsp/information.jsp?sname=C_ID&word='
+            if 'knapsackId' in datum.keys():
+                datum['knapsackId'] = 'C'+datum['knapsackId']        
 
-        aList = soup.find_all('a')
-        for a in aList:
-            href = str(a.get('href'))
-            for k,h in hrefDict.iteritems():
-                if h in href:
-                    datum[k] = href.strip(h)
-    
-        if 'knapsackId' in datum.keys():
-            datum['knapsackId'] = 'C'+datum['knapsackId']        
-
-        divList = soup.find_all('div')
-        for d in divList:
-            v = str(d.get('style'))
-            if 'margin-left:3em' in v:
-                d = str(d).strip('<div style="margin-left:3em">').strip('</div>')
-                datum['casId'] = d
+            divList = soup.find_all('div')
+            for d in divList:
+                v = str(d.get('style'))
+                if 'margin-left:3em' in v:
+                    d = str(d).strip('<div style="margin-left:3em">').strip('</div>')
+                    datum['casId'] = d
 
         data[idStr] = datum
 
-        if ((i+1)%10)==0 or i==(n-1):
+        if ((i+1)%100)==0 or i==(n-1):
             jsonFpath = baseDir+'/kegg_compound_data_'+str(now.date())+'_'+str(now.time())+'.json'
             with open(jsonFpath, 'w') as f:
                 json.dump(data, f, indent=2, sort_keys=True)
