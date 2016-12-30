3a408520cbfd67700048d564c9e7654686066861
hzb-imasine <antares.zenithal@gmail.com>
2016-12-30 18:08:41 +0700

    fix deploy

--- ijah-webserver/deploy.sh
+++ ijah-webserver/deploy.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
- #build the src in production stage
+#build the src in production stage
+echo "building ..."
 npm run build:prod
 
 # backup existing content of ijah at apps.cs
+echo "backing up ..."
 stamp=`date +%Y-%m-%d-%H-%M-%S`
-backdir='/var/www/ijah/backup_'
+backdir='/home/tor/ijah-backup/ijah-backup_'
 cmd='mkdir '
 cmd2=$cmd$backdir$stamp
 
 ssh tor@apps.cs.ipb.ac.id $cmd2
-
-ssh tor@apps.cs.ipb.ac.id find ijah/ -type f ! -name "backup*" -exec cp --parents -t $backdir$stamp/ {} +
+ssh tor@apps.cs.ipb.ac.id find /var/www/ijah/ -type f ! -name "backup*" -exec cp --parents -t $backdir$stamp/ {} +
 	
-# #upload
+# upload
+echo "uploading files ..."
 scp -r dist/* tor@apps.cs.ipb.ac.id:/var/www/ijah/
 scp -r src/css tor@apps.cs.ipb.ac.id:/var/www/ijah/
 scp -r src/img tor@apps.cs.ipb.ac.id:/var/www/ijah/
 scp -r src/output.html tor@apps.cs.ipb.ac.id:/var/www/ijah/
-#scp -r /var/www/html/ijah
+scp -r api/* tor@apps.cs.ipb.ac.id:/var/www/ijah/ijah
