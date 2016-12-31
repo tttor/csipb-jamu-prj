b56c7cb9d27f7a79e74efc7ed9f6e190276ce7a5
Vektor Dewanto <vektor.dewanto@gmail.com>
2016-12-31 15:01:48 +0700

    download plantList.txt

--- ijah-webserver/src/app/download/download.component.ts
+++ ijah-webserver/src/app/download/download.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Http } from '@angular/http';
+
+declare var saveAs: any;
 
 @Component({
   selector: 'download',
@@ -9,7 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class Download {
   localState;
-  constructor(public route: ActivatedRoute) {
+  constructor(public route: ActivatedRoute, private http: Http) {
 
   }
 
@@ -19,6 +22,25 @@ export class Download {
   }
   asyncDataWithWebpack() {
 
+  }
+
+  downloadList(type,fname) {
+    console.log('downloadList')
+
+    // Query Data from DB
+    this.http.get('http://ijah.apps.cs.ipb.ac.id/ijah/plant.php')
+      .map(res => res.json())
+      .subscribe(data => {
+        let list: string[] = [];
+        for (let i = 0; i < data.length; i++) {
+          list.push(data[i]['pla_name']);
+        }
+
+        // Download
+        let str = list.join("\n");
+        let blob = new Blob([str], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, 'ijah_plantList.txt');
+      })
   }
 
 }
