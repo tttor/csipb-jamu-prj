42dfd06729bdb2ec45535167d550b278f794e814
hzb-imasine <antares.zenithal@gmail.com>
2016-12-31 19:25:15 +0700

    download function and api

--- ijah-webserver/api/query_pro_dis.php
+++ ijah-webserver/api/query_pro_dis.php
@@ -1,6 +1,6 @@
 <?php
   include 'config.php';
-  $query = pg_query($link, "SELECT * FROM compound_vs_protein");
+  $query = pg_query($link, "SELECT * FROM protein_vs_disease");
 
   $array = array();
   while($row = pg_fetch_assoc($query)){
