c71b24341dd60ca8dc1d2f756bf61af064ec63f6
ajmalkurnia <ajmalprayoga@gmail.com>
2017-02-06 13:15:34 +0700

    PHP socket client and some minor change on server side

--- webserver/api/predict.php
+++ webserver/api/predict.php
@@ -6,17 +6,49 @@
 
   foreach($requestList as $req){
     //PHP will call python each pair one by one
-    //Python will return string in format=com_id|pro_id|weight|source|time_stamp
-    //Notice: somehow, we _cannot_ execute python file under git repo
-    $command = escapeshellcmd("python ".$predictorClient." ".$req['comId']." ".$req['proId']);
-    $output = shell_exec($command);
+    //Python will return string in format=weight|source|time_stamp
 
+    /*--- Move this to config ---*/
+    $service_port = 5558;
+    $address = '127.0.0.1';
+    /*---------------------------*/
+
+    //---------Socket Part---------//
+    //Create Socket
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    if ($socket === false){
+      echo "socket_create() failed: reason:". socket_strerror(socket_last_error())."\n";
+      continue;
+    }
+    //Connect to Server
+    $result = socket_connect($socket, $address, $service_port);
+    if ($result === false){
+      echo "socket_connect() failed.\nReason:($result) ".socket_strerror(socket_last_error($socket))."\n";
+      continue;
+    }
+    //Preparing Input and output
+    $in = $req['comId'].":".$req['proId'];
+    $in .= "|end";
+
+    $sockOut = '';
+    $output = "";
+
+    //Sending data
+    socket_write($socket, $in, strlen($in));
+
+    //Catch Data
+    while($sockOut = socket_read($socket, 2048)){
+      $output .= $sockOut;
+    }
+    socket_close($socket);
+
+    //--------Preparing Output--------//
     if ($output!==null) {
       $output = explode("|",$output);
-      $source = $output[3];
-      $weight = trim($output[2]);
-      $timestamp = $output[4];
-      $row = array('com_id'=>$output[0],'pro_id'=>$output[1],
+      $source = $output[1];
+      $weight = trim($output[0]);
+      $timestamp = $output[2];
+      $row = array('com_id'=>$req['comId'],'pro_id'=>$req['proId'],
                    'weight'=>$weight,'source'=>$source,'timestamp'=>$timestamp);
       $respArr[] = $row;
     }
