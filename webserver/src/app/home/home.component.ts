d101359afbaa117d569e16e70e2994e6f939e996
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-02-09 11:29:21 +0700

    Put the number of connected entities in text output #91

--- webserver/src/app/home/home.component.ts
+++ webserver/src/app/home/home.component.ts
@@ -724,6 +724,8 @@ export class Home implements OnInit {
     let destPropKeys = this.getPropKeys(destType);
 
     let nUnique = 0;
+    let nUniquePerConnSrc = 0;
+    let pos = -1;// in which, we will insert nUniquePerConnSrc
     let prevSrc = '';
     let prevConnSource = '';
     for(let i=0;i<interaction.length;i++) {
@@ -733,6 +735,8 @@ export class Home implements OnInit {
       let weight = interaction[i]['weight'];
 
       if (prevSrc!==src) {
+        text = [text.slice(0,pos),nUniquePerConnSrc.toString(), text.slice(pos)].join('');
+
         nUnique = nUnique + 1;
         text = text+'#'+nUnique.toString()+' ';
 
@@ -745,8 +749,13 @@ export class Home implements OnInit {
       }
 
       if (prevConnSource!==source) {
-        text += indent+'['+source+']:\n';
+        text += indent+'['+source+':]\n';
+        pos = text.length - 2;
         prevConnSource = source;
+        nUniquePerConnSrc = 1;
+      }
+      else {
+        nUniquePerConnSrc += 1;
       }
 
       let destProps = this.getProps(dest,destPropKeys,destMeta);
@@ -1017,24 +1026,24 @@ export class Home implements OnInit {
     headerArr['pro'] = 'UniprotID|UniprotName|PDBId(s)';
     headerArr['dis'] = 'OmimID|OmimName';
 
-    headerArr['pla_vs_com'] ='#0 '+headerArr['pla']+'\n'+
-                              indent+'[source]:'+'\n'+
+    headerArr['pla_vs_com'] ='#0 '+headerArr['pla']+':\n'+
+                              indent+'[source]'+'\n'+
                               indent+'[weight] '+headerArr['com'];
-    headerArr['com_vs_pro'] = '#0 '+headerArr['com']+'\n'+
-                              indent+'[source]:'+'\n'+
+    headerArr['com_vs_pro'] = '#0 '+headerArr['com']+':\n'+
+                              indent+'[source]'+'\n'+
                               indent+'[weight] '+headerArr['pro'];
-    headerArr['pro_vs_dis'] = '#0 '+headerArr['pro']+'\n'+
-                              indent+'[source]:'+'\n'+
+    headerArr['pro_vs_dis'] = '#0 '+headerArr['pro']+':\n'+
+                              indent+'[source]'+'\n'+
                               indent+'[weight] '+headerArr['dis'];
 
-    headerArr['com_vs_pla'] ='#0 '+headerArr['com']+'\n'+
-                              indent+'[source]:'+'\n'+
+    headerArr['com_vs_pla'] ='#0 '+headerArr['com']+':\n'+
+                              indent+'[source]'+'\n'+
                               indent+'[weight] '+headerArr['pla'];
-    headerArr['pro_vs_com'] = '#0 '+headerArr['pro']+'\n'+
-                              indent+'[source]:'+'\n'+
+    headerArr['pro_vs_com'] = '#0 '+headerArr['pro']+':\n'+
+                              indent+'[source]'+'\n'+
                               indent+'[weight] '+headerArr['com'];
-    headerArr['dis_vs_pro'] = '#0 '+headerArr['dis']+'\n'+
-                              indent+'[source]:'+'\n'+
+    headerArr['dis_vs_pro'] = '#0 '+headerArr['dis']+':\n'+
+                              indent+'[source]'+'\n'+
                               indent+'[weight] '+headerArr['pro'];
 
     return headerArr[type];
