922f01bb63f3b559f6d570e3153a1d0acb9ee7be
ajmalkurnia <ajmalprayoga@gmail.com>
2017-02-10 16:34:38 +0700

    fix simprot

--- predictor/deprecated/run_sim_prot.sh
+++ predictor/deprecated/run_sim_prot.sh
@@ -4,15 +4,14 @@ END_INDEX=$2
 STEP=$3
 COLUMN_STEP=$4
 
-FLAG="-m scoop"
-EXE="Sim_Prot.py"
+EXE="sim_Prot.py"
 
 for (( i=$START_INDEX; i<$END_INDEX ;i+=$STEP))
 do
     TEMP=$[$i+$STEP]
     if [ $END_INDEX -gt $TEMP ]; then
-      python $FLAG $EXE $i $TEMP $COLUMN_STEP
+      python $EXE $i $TEMP $COLUMN_STEP
     else
-      python $FLAG $EXE $i $END_INDEX $COLUMN_STEP
+      python $EXE $i $END_INDEX $COLUMN_STEP
     fi
 done
