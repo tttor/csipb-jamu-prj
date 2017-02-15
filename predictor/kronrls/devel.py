ac7a07cda9eddd0d403f60049eaf6cf6d9c71a3a
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-02-15 16:10:04 +0700

    calculate perf and plot

--- predictor/kronrls/devel.py
+++ predictor/kronrls/devel.py
@@ -1,13 +1,19 @@
 # devel.py
-import yamanishi_data_util as yam
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
 from kronrls import KronRLS
 from sklearn.cross_validation import KFold
 from sklearn.cross_validation import StratifiedKFold
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+import yamanishi_data_util as yam
+
+outDir = '../../xprmt'
 
 def main(argv):
     if len(argv)!=2:
-        print 'USAGE: python devel.py [valType]'
+        print 'USAGE: python devel.py [valMode]'
         return
 
     valMode = argv[1]
@@ -36,11 +42,13 @@ def main(argv):
         nFolds = nData
         kfList = KFold(nData, n_folds=nFolds, shuffle=True)
     elif valMode=='kfcv':
-        nFolds = 10
+        nFolds = 5
         kfList = StratifiedKFold(dataY, n_folds=nFolds, shuffle=True)
     else:
         assert(False)
 
+    yTestList = []
+    yPredList = []
     fold = 0
     for trIdxList, testIdxList in kfList:
         fold += 1
@@ -54,6 +62,31 @@ def main(argv):
         # test
         gamma = 1.0
         yPred = kronrls.predict(xTest,gamma)
+
+        yTestList += yTest
+        yPredList += yPred
+
+
+    ##
+    precision, recall, _ = precision_recall_curve(yTestList, yPredList)
+    prAUC = average_precision_score(yTestList, yPredList, average='micro')
+
+    ##
+    plt.clf()
+    plt.figure()
+
+    plt.plot(recall, precision, 'r-',
+             label= '(area = %0.2f)' % prAUC, lw=2)
+
+    plt.ylim([-0.05, 1.05])
+    plt.xlim([-0.05, 1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision-Recall Curve')
+    plt.legend(loc="lower left")
+
+    fname = '/pr_curve_'+valMode+'.png'
+    plt.savefig(outDir+fname, bbox_inches='tight')
 
 if __name__ == '__main__':
     main(sys.argv)
