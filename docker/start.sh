4206f8413806a5a698ae16f39d3a90dc3df15acc
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-02-23 05:34:30 +0700

    passing arg to starting scripts

--- docker/start.sh
+++ docker/start.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
+################################################################################
 echo "Starting IJAH web..."
 docker run --restart=always -d --name "ijahweb_daemon" -p 2002:80 -v /home/ijah/ijah/web:/var/www/html ijah
 
+################################################################################
 echo "Starting IJAH load balancer..."
-docker run --restart=always -d --name "ijah_loadbalancer_daemon" -p 5000:5000 -v /home/ijah/ijah-predictor/python:/ijah-predictor ijahpredictor python -u /ijah-predictor/server/load_balancer.py 0.0.0.0 5000 172.18.79.22 5010 5015 5
 
+waitingTime=5
+phpApiHost=0.0.0.0
+phpApiPort=5000
+serverHost=172.18.79.22
+serverPortLo=5010
+serverPortHi=5015
+LB=/ijah-predictor/server/load_balancer.py
+
+docker run --restart=always -d --name "ijah_loadbalancer_daemon" -p $phpApiPort:phpApiPort -v /home/ijah/ijah-predictor/python:/ijah-predictor ijahpredictor python -u $LB $phpApiHost $phpApiPort $serverHost $serverPortLo $serverPortHi $waitingTime
+
+################################################################################
 echo "Starting IJAH predictor server 0"
-docker run --restart=always -d --name "ijah_predictor_0_daemon" -p 5010-5012:5010-5012 -v /home/ijah/ijah-predictor/python:/ijah-predictor ijahpredictor python -u /ijah-predictor/server/server.py 0 0.0.0.0 5010 5012
 
+serverId=0
+LBHostToListenFrom=0.0.0.0
+LBPortToListenFromLo=5010
+LBPortToListenFromHi=5012
+server=/ijah-predictor/server/server.py
+
+docker run --restart=always -d --name "ijah_predictor_0_daemon" -p $LBPortToListenFromLo-$LBPortToListenFromHi:$LBPortToListenFromLo-$LBPortToListenFromHi -v /home/ijah/ijah-predictor/python:/ijah-predictor ijahpredictor python -u $server $serverId $LBHostToListenFrom $LBPortToListenFromLo $LBPortToListenFromHi
