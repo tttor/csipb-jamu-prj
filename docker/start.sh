c065026369473882c7ee0762216aec5407aa0eba
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-02-23 09:47:05 +0700

    fix typo var

--- docker/start.sh
+++ docker/start.sh
@@ -15,7 +15,7 @@ serverPortLo=5010
 serverPortHi=5015
 LB=/ijah-predictor/server/load_balancer.py
 
-docker run --restart=always -d --name "ijah_loadbalancer_daemon" -p $phpApiPort:phpApiPort -v /home/ijah/ijah-predictor/python:/ijah-predictor ijahpredictor python -u $LB $phpApiHost $phpApiPort $serverHost $serverPortLo $serverPortHi $waitingTime
+docker run --restart=always -d --name "ijah_loadbalancer_daemon" -p $phpApiPort:$phpApiPort -v /home/ijah/ijah-predictor/python:/ijah-predictor ijahpredictor python -u $LB $phpApiHost $phpApiPort $serverHost $serverPortLo $serverPortHi $waitingTime
 
 ################################################################################
 echo "Starting IJAH predictor server 0"
