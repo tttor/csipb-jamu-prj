b00a52f5ae639f89cabfb9bfadae0f6c2477b31a
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-02-24 09:21:38 +0700

    add deploying docker files as an  option

--- docker/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-scp start.sh stop.sh ijah@ijahserver:/home/ijah/
-
-#
-# scp webDockerfile ijah@ijahserver:/home/ijah/ijah/
-# scp predictorDockerfile ijah@ijahserver:/home/ijah/ijah-predictor
