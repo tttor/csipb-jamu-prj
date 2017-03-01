49bc46533367131abb0e4cda8f2be3a3fbd29f00
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-03-01 21:44:42 +0700

    add quote funct

--- database/inserter/postgresql_util.py
+++ database/inserter/postgresql_util.py
@@ -2,6 +2,9 @@
 import sys
 import psycopg2
 
+def quote(s):
+    return "'"+s+"'"
+
 def getMax(csr,col,table):
     csr.execute("SELECT MAX("+col+") FROM "+table)
     _ = csr.fetchall(); assert len(_)==1
