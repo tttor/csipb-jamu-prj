5f230e4fef13a969c99cfe17bdd16937261ed2e9
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-03-04 06:38:43 +0700

    add untracked credential

---
+++ config/database_config.py
@@ -0,0 +1,10 @@
+# database_config.py
+from credential import *
+
+databaseConfigServer = dict(name='ijah',user='ijah',passwd=DB_PASSWD,
+                            host='192.168.254.1',port='5432')
+databaseConfigLocal = dict(name='ijah',user='ijah',passwd=DB_PASSWD_LOCAL,
+                            host='localhost',port='5432')
+
+databaseConfig = databaseConfigServer
+# databaseConfig = databaseConfigLocal
