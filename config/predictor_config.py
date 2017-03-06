0b3ac87e00b63cfdba234c32ae9ede00cde1c071
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-03-06 17:37:12 +0700

    lower threshold in kronrls

--- config/predictor_config.py
+++ config/predictor_config.py
@@ -4,7 +4,7 @@
 rndlyConfig = dict(name='rndly',weight=0.5,batchSize=1)
 kronRLSConfig = dict(name='kronrls',weight=0.6,
                      batchSize=10,maxTrainingDataSize=100,
-                     gamma=1.0,threshold=0.5,
+                     gamma=1.0,threshold=0.1,
                      alphaCompoundKernel=0.5,alphaProteinKernel=0.5,
                      kernelBandwidth=1.0)
 blmniiConfig = dict(name='blmnii',weight=0.6,batchSize=1,
