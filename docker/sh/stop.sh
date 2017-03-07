45a029cc051bc16ffcd065072d1fe3adff77691a
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-03-07 15:04:26 +0700

    add stop for other predictor daemons

--- docker/sh/stop.sh
+++ docker/sh/stop.sh
@@ -11,3 +11,9 @@ docker rm   ijah_loadbalancer_daemon
 echo "Stopping IJAH predictor..."
 docker stop ijah_predictor_0_daemon
 docker rm   ijah_predictor_0_daemon
+docker stop ijah_predictor_1_daemon
+docker rm   ijah_predictor_1_daemon
+docker stop ijah_predictor_2_daemon
+docker rm   ijah_predictor_2_daemon
+docker stop ijah_predictor_3_daemon
+docker rm   ijah_predictor_3_daemon
