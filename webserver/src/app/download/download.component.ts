713d7cd98824292ed895439c6e6aa3c5f950db85
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-04-11 17:33:27 +0700

    default to serverAPI

--- webserver/src/app/download/download.component.ts
+++ webserver/src/app/download/download.component.ts
@@ -14,8 +14,8 @@ export class DownloadComponent implements OnInit {
   private baseAPI;
   constructor(public route: ActivatedRoute, private http: Http) {
     // this.baseAPI = 'http://ijah.apps.cs.ipb.ac.id/api/';
-    // this.baseAPI = 'http://ijah.agri.web.id/api/';
-    this.baseAPI = 'http://localhost/ijah-api/'; // Comment this if you run online!
+    this.baseAPI = 'http://ijah.agri.web.id/api/';
+    // this.baseAPI = 'http://localhost/ijah-api/'; // Comment this if you run online!
   }
 
   public ngOnInit() {
