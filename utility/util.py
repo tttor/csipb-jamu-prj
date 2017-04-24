a8fa73935ce917ca9dfe6df529d4eb98e7c22e5c
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-04-25 05:04:20 +0700

    add makeKernelMat

--- utility/util.py
+++ utility/util.py
@@ -2,7 +2,7 @@
 import numpy as np
 import sys
 
-def kernel2distanceMatrix(simMat,method):
+def kernel2distanceMatrix(method,simMat):
     # https://rdrr.io/cran/mmpp/man/k2d.html
     m,n = simMat.shape
     disMat = np.ones((m,n),dtype=float)
@@ -16,6 +16,22 @@ def kernel2distanceMatrix(simMat,method):
         assert False, 'FATAL: unkown method'
 
     return disMat
+
+def makeKernelMatrix(kernelDict,comList,proList):
+    nCom = len(comList); nPro = len(proList)
+
+    comSimMat = np.zeros((nCom,nCom),dtype=float)
+    proSimMat = np.zeros((nPro,nPro),dtype=float)
+
+    for row,i in enumerate(comList):
+        for col,j in enumerate(comList):
+            comSimMat[row][col] = kernelDict[(i,j)]
+
+    for row,i in enumerate(proList):
+        for col,j in enumerate(proList):
+            proSimMat[row][col] = kernelDict[(i,j)]
+
+    return (comSimMat,proSimMat)
 
 def getType(idStr):
     prefix1 = idStr[0:1]
