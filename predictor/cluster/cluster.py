f49af6be16b00ffc7ac486b00457f5bf17d95042
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-04-30 11:55:30 +0700

    paralel on tuning on clustering

--- predictor/cluster/cluster.py
+++ predictor/cluster/cluster.py
@@ -3,34 +3,28 @@ import os
 import sys
 import time
 import json
+import numpy as np
 import sklearn.metrics as met
-from sklearn.model_selection import RandomizedSearchCV as rscv
+from scoop import futures as fu
+from scoop import shared as sh
 
 sys.path.append('../../utility')
 import util
 import yamanishi_data_util as yam
 
+# np.random.seed(0)
+
 def main():
-    if len(sys.argv)!=5:
+    if len(sys.argv)!=6:
         print 'USAGE:'
-        print 'python cluster.py [method] [dataset] [dataDir] [outDir]'
+        print 'python -m scoop cluster.py [method] [nIter] [dataset] [dataDir] [outDir]'
         return
 
     method = sys.argv[1]
-    dataset = sys.argv[2]
-    dataDir = sys.argv[3]
-    outDir = sys.argv[4]
-
-    ##
-    cls = None
-    if method=='kmedoid':
-        from kmedoid import kmedoid
-        cls = kmedoid
-    elif method=='dbscan':
-        from sklearn.cluster import DBSCAN
-        cls = DBSCAN(eps=0.75,min_samples=1,metric='precomputed')
-    else:
-        assert False, 'FATAL: unknown cluster method'
+    nIter = int(sys.argv[2])
+    dataset = sys.argv[3]
+    dataDir = sys.argv[4]
+    outDir = sys.argv[5]
 
     ##
     _,comList,proList = yam.loadComProConnMat(dataset,os.path.join(dataDir,'ground-truth'))
@@ -39,35 +33,90 @@ def main():
     comDisMat,proDisMat = list(map(util.kernel2distanceMatrix,['naive']*2,[comSimMat,proSimMat]))
 
     ##
-    random_search = rscv(cls, param_distributions=param_dist,
-                                        n_iter=n_iter_search)
-
-start = time()
-random_search.fit(X, y)
-print("RandomizedSearchCV took %.2f seconds for %d candidates"
-      " parameter settings." % ((time() - start), n_iter_search))
-report(random_search.cv_results_)
-
-    ##
     print 'clustering...'
-    comLabels,proLabels = list( map(cls.fit_predict,[comDisMat,proDisMat]) )
-    comLabels = comLabels.tolist(); proLabels = proLabels.tolist()
+    paramSpace = {"eps": [0.0,1.0],"min_samples":[1,5]}
+    paramList = _elaborateParamSpace(paramSpace,nIter,method)
+
+    sh.setConst(method=method)
+    sh.setConst(mat=comDisMat)
+
+    comResList = list( fu.map(_cluster,paramList) )
+    bestResIdxCal = _getBestResultIdx(comResList,'calinski_harabaz_score')
+    bestResIdxSil = _getBestResultIdx(comResList,'silhouette_score')
+
+    resDictCal = dict( zip(comList,comResList[bestResIdxCal][0]) )
+    resDictSil = dict( zip(comList,comResList[bestResIdxSil][0]) )
+
+    bestParamCal = dict(param=paramList[bestResIdxCal],
+                        score=comResList[bestResIdxCal][1])
+    bestParamSil = dict(param=paramList[bestResIdxSil],
+                        score=comResList[bestResIdxSil][1])
 
     ##
-    print 'eval...'
-    comSil,proSil = list(map(met.silhouette_score,
-                            [comDisMat,proDisMat],[comLabels,proLabels],
-                            ['precomputed']*2))
-    comCal,proCal = list(map(met.calinski_harabaz_score,
-                            [comDisMat,proDisMat],[comLabels,proLabels]))
+    fname = method+"_"+'calinskiharabazscore'+"_clusterlabels.json"
+    with open(os.path.join(outDir,fname),'w') as f:
+        json.dump(resDictCal,f,indent=2,sort_keys=True)
 
-    perf = dict(comCluster={'silhouette_score':comSil,'calinski_harabaz_score':comCal},
-                proCluster={'silhouette_score':proSil,'calinski_harabaz_score':proCal})
+    fname = method+"_"+'silhouettescore'+"_clusterlabels.json"
+    with open(os.path.join(outDir,fname),'w') as f:
+        json.dump(resDictSil,f,indent=2,sort_keys=True)
 
-    with open(os.path.join(outDir+"perf.json"),'w') as f:
-        json.dump(perf,f,indent=2,sort_keys=True)
+    fname = method+"_"+'calinskiharabazscore'+"_bestparam.json"
+    with open(os.path.join(outDir,fname),'w') as f:
+        json.dump(bestParamCal,f,indent=2,sort_keys=True)
+
+    fname = method+"_"+'silhouettescore'+"_bestparam.json"
+    with open(os.path.join(outDir,fname),'w') as f:
+        json.dump(bestParamSil,f,indent=2,sort_keys=True)
+
+def _cluster(params):
+    cls = None
+    method = sh.getConst('method')
+    if method=='kmedoid':
+        assert False
+        # from kmedoid import kmedsoid
+        # cls = kmedoid
+    elif method=='dbscan':
+        from sklearn.cluster import DBSCAN
+        cls = DBSCAN(eps=params['eps'],min_samples=params['min_samples'],
+                     metric='precomputed')
+    else:
+        assert False, 'FATAL: unknown cluster method'
+
+    ##
+    mat = sh.getConst('mat')
+    labels = cls.fit_predict(mat)
+    nLabels = len(set(labels))
+
+    ##
+    sil = None; cal = None
+    if (nLabels >= 2)and(nLabels <= len(labels)-1):
+        sil = met.silhouette_score(mat,labels,'precomputed')
+        cal = met.calinski_harabaz_score(mat,labels)
+    perf = dict(silhouette_score=sil,calinski_harabaz_score=cal)
+
+    return (labels,perf)
+
+def _elaborateParamSpace(paramSpace,nIter,method):
+    paramList = []
+    for i in range(nIter):
+        if method=='dbscan':
+            epsMin,epsMax = paramSpace['eps']
+            nMin,nMax = paramSpace['min_samples']
+
+            eps = np.random.uniform(epsMin,epsMax,1)[0]
+            n = np.random.randint(nMin,nMax,1)[0]
+            param = dict(eps=eps,min_samples=n)
+            paramList.append(param)
+        else:
+            assert False
+    return paramList
+
+def _getBestResultIdx(resList,metric):
+    mets = [i[1][metric] for i in resList]
+    return mets.index(max(mets))
 
 if __name__ == '__main__':
     tic = time.time()
     main()
-    print "main: "+str(time.time()-tic)+' seconds'
+    print "main took: "+str(time.time()-tic)+' seconds'
