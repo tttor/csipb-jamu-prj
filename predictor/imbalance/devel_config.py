9aa7d125c8249dd9c31cc9cd2b93c6bf64d76469
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-05-25 10:49:32 +0700

    fix default devel config

--- predictor/imbalance/devel_config.py
+++ predictor/imbalance/devel_config.py
@@ -1,9 +1,9 @@
 # devel_config.py
 config = dict(method='esvm',mode='hard',bootstrap=True,
               nClone=3, testSize=0.30,
-              maxTrainingSamplesPerBatch=100,
-              maxTestingSamplesPerBatch=100,maxTestingSamples=100,
+              maxTrainingSamplesPerBatch=1000,
+              maxTestingSamplesPerBatch=100,maxTestingSamples=0,
               clusterMetric='calinskiharabaz',
-              clusterDir='../cluster/output/cluster006-yamanishi#e',
+              clusterDir='../cluster/output/cluster001-yamanishi#e',
               datasetDir='../../dataset/connectivity/compound_vs_protein'
               )
