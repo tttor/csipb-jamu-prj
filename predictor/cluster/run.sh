563c19dd1ac251e194278b1e1e120d3050bb698b
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-05-25 09:59:07 +0700

    fix cluster2 stat

--- predictor/cluster/run.sh
+++ predictor/cluster/run.sh
@@ -7,8 +7,9 @@ fi
 
 N_ITER=10
 METHOD=dbscan
-DATASET=$1
+GRAPHIC=0
 
+DATASET=$1
 OUTDIR=output/$2-$DATASET
 rm -rf $OUTDIR
 mkdir $OUTDIR
@@ -18,3 +19,7 @@ python -m scoop cluster.py $METHOD $N_ITER $DATASET compound $OUTDIR
 
 echo '### cluster protein #####################################################'
 python -m scoop cluster.py $METHOD $N_ITER $DATASET protein $OUTDIR
+
+echo '### cluster into neg,pos,unlabel #########################################'
+python cluster2.py cal $OUTDIR $GRAPHIC
+python cluster2.py sil $OUTDIR $GRAPHIC
