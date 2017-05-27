3e87e83926fe928e97afb3fce0335293ed20b699
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-05-27 18:26:50 +0700

    plot confusion mat

--- predictor/imbalance/anal.py
+++ predictor/imbalance/anal.py
@@ -3,10 +3,13 @@ import os
 import sys
 import pickle
 import json
+import itertools
+import numpy as np
 from collections import defaultdict as ddict
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import average_precision_score
+import matplotlib.pyplot as plt
 
 def main():
    if len(sys.argv)!=3:
@@ -24,12 +27,42 @@ def main():
    dirs = [i for i in os.listdir(tdir) if (tag in i)and('anal' not in i)]; assert len(dirs)>0
    for d in dirs:
       with open(os.path.join(tdir,d,'result.pkl'),'r') as f: result = pickle.load(f)
-      ytrue = result['yte']; ypred = result['ypred']; yscore = result['yscore']
+      ytrue = result['yte']; ypred = result['ypred']; yscore = result['yscore']; labels = list(set(ytrue))
       perfs['roc_auc_score'].append( roc_auc_score(ytrue,yscore,average='macro') )
       perfs['aupr_score'].append( average_precision_score(ytrue,yscore,average='macro') )
       cms.append( confusion_matrix(ytrue,ypred) )
 
+   def _getBestIdx(metric):
+      idx = perfs[metric].index( max(perfs[metric]) )
+      return idx
+
+   for m in perfs.keys():
+      for n in ['normalized','unnormalized']:
+         _plotCM(cms[_getBestIdx(m)],labels,n,os.path.join(odir,'cm_best_'+m+'_'+n+'.png'))
+
    with open(os.path.join(odir,'perfs.json'),'w') as f: json.dump(perfs,f,indent=2,sort_keys=True)
+
+def _plotCM(cm,classes,normalized,fpath):
+   """
+   This function prints and plots the confusion matrix.
+   Normalization can be applied by setting `normalize=True`.
+   """
+   fig = plt.figure()
+   plt.imshow(cm, interpolation='nearest',cmap=plt.cm.Blues)
+   plt.colorbar()
+   tick_marks = np.arange(len(classes))
+   plt.xticks(tick_marks, classes, rotation=45)
+   plt.yticks(tick_marks, classes)
+   if normalized=='normalized': cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+   thresh = cm.max() / 2.
+   for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+     plt.text(j, i, cm[i, j],
+              horizontalalignment="center",
+              color="white" if cm[i, j] > thresh else "black")
+   plt.ylabel('True label')
+   plt.xlabel('Predicted label')
+   plt.savefig(fpath,dpi=300,format='png',bbox_inches='tight')
+   plt.close(fig)
 
 if __name__ == '__main__':
    main()
