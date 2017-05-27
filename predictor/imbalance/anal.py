f3d304540017b3d1a91f9f537f63d82bd2e71500
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-05-27 17:17:28 +0700

    roc and aupr

--- predictor/imbalance/anal.py
+++ predictor/imbalance/anal.py
@@ -4,10 +4,9 @@ import sys
 import pickle
 import json
 from collections import defaultdict as ddict
-from sklearn.metrics import matthews_corrcoef
-from sklearn.metrics import cohen_kappa_score
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import fbeta_score
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import average_precision_score
 
 def main():
    if len(sys.argv)!=3:
@@ -23,13 +22,11 @@ def main():
 
    perfs = ddict(list); cms = []
    dirs = [i for i in os.listdir(tdir) if (tag in i)and('anal' not in i)]; assert len(dirs)>0
-   print dirs
    for d in dirs:
       with open(os.path.join(tdir,d,'result.pkl'),'r') as f: result = pickle.load(f)
-      ytrue = result['yte']; ypred = result['ypred']
-      perfs['matthews_corrcoef'].append( matthews_corrcoef(ytrue,ypred) )
-      perfs['cohen_kappa_score'].append( cohen_kappa_score(ytrue,ypred) )
-      perfs['fbeta_score'].append( fbeta_score(ytrue,ypred,average='binary',beta=0.5) )
+      ytrue = result['yte']; ypred = result['ypred']; yscore = result['yscore']
+      perfs['roc_auc_score'].append( roc_auc_score(ytrue,yscore,average='macro') )
+      perfs['average_precision_score'].append( average_precision_score(ytrue,yscore,average='macro') )
       cms.append( confusion_matrix(ytrue,ypred) )
 
    with open(os.path.join(odir,'perfs.json'),'w') as f: json.dump(perfs,f,indent=2,sort_keys=True)
