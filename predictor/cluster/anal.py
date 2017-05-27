9c9ee7b962db5dbb13fcf2a3601e25312ff13963
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-05-27 10:42:36 +0700

    plot scatter com, pro cls

--- predictor/cluster/anal.py
+++ predictor/cluster/anal.py
@@ -17,7 +17,34 @@ def main():
    odir = os.path.join(tdir,'anal')
    if not os.path.exists(odir): os.makedirs(odir)
 
-   _pie(tdir,odir)
+   # _pie(tdir,odir)
+   _scatter(tdir,odir)
+
+def _scatter(tdir,odir):
+   odir = os.path.join(odir,'scatter')
+   if not os.path.exists(odir): os.makedirs(odir)
+
+   for d in [i for i in os.listdir(tdir) if not('anal' in i)]:
+      fig = plt.figure()
+      for d2 in [i for i in os.listdir(os.path.join(tdir,d)) if os.path.isdir(os.path.join(tdir,d,i))]:
+         print d2
+         dataset = d2.split('-')[2]
+         c = 'r' if ('protein' in d2) else 'g'
+         for fname in [i for i in os.listdir(os.path.join(tdir,d,d2)) if 'bestlabels_stat' in i]:
+            m = '^' if 'silhouette' in fname else 'o'
+            # tag = 'protein' if ('protein' in d2) else 'compound'
+            # tag = '_silhouette' if 'silhouette' in fname else '_calinskiharabaz'
+            with open(os.path.join(tdir,d,d2,fname),'r') as f:
+               data = yaml.load(f)
+               x = [int(i) for i in data.keys()]
+               y = [i[0] for i in data.values()]
+               plt.scatter(x,y,c=[c]*len(x),alpha=0.5,marker=m)
+               plt.grid(True)
+               plt.xlabel('class labels')
+               plt.ylabel('#members')
+               plt.savefig(os.path.join(odir,dataset+'_scatter.png'),
+                           dpi=300,format='png',bbox_inches='tight')
+      plt.close(fig)
 
 def _pie(tdir,odir):
    for d in [i for i in os.listdir(tdir) if not('anal' in i)]:
