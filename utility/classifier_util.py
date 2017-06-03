cf42e1c295d93a71d868fbb29691f9f26ff6368a
Vektor Dewanto <vektor.dewanto@gmail.com>
2017-06-03 07:59:09 +0700

    add divideSamples()

--- utility/classifier_util.py
+++ utility/classifier_util.py
@@ -3,6 +3,30 @@ import os
 import numpy as np
 from scoop import futures as fu
 from scoop import shared as sh
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import KFold
+
+def divideSamples(x,y,maxSamplesPerBatch):
+   nSplits = ( len(x)/maxSamplesPerBatch ) + 1
+   if nSplits==1:# take all
+      idxesList = [ range(len(x)) ]
+   else:# abusely use StratifiedKFold, taking only the testIdx
+      if y is None:
+         cv = KFold(n_splits=nSplits)
+         idxesList = [testIdx for  _, testIdx in cv.split(x) ]
+      else:
+         cv = StratifiedKFold(n_splits=nSplits,shuffle=True)
+         idxesList = [testIdx for  _, testIdx in cv.split(x,y) ]
+
+   ##
+   xyList = []
+   for idxes in idxesList:
+      xList = [x[i] for i in idxes]
+      if y is None: yList = None
+      else: yList = [y[i] for i in idxes]
+      xyList.append( (xList,yList) )
+
+   return xyList
 
 def loadFeature(x,comFeaDir,proFeaDir):
    sh.setConst(comFeaDir=comFeaDir)
