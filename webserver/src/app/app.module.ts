c51de2ca1a2b7c5c2efc722243702fba7a12fcd8
hzb-imasine <antares.zenithal@gmail.com>
2017-08-30 15:14:16 +0700

    init contents for ng4 migration

--- webserver/src/app/app.module.ts
+++ webserver/src/app/app.module.ts
@@ -2,25 +2,48 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ApplicationRef } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http, RequestOptions } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, PreloadAllModules } from '@angular/router';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { CommonModule } from "@angular/common";
 import { enableProdMode } from '@angular/core';
 
+import { Ng2PageScrollModule } from 'ng2-page-scroll';
+import { FileUploadModule } from 'ng2-file-upload';
+import { ChartsModule } from 'ng2-charts';
+import { TreeviewModule } from 'ngx-treeview';
+
 import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DownloadComponent } from './download/download.component';
+import { HelpComponent } from './help/help.component';
+import { Ijahv1Component } from './ijahv1/ijahv1.component';
+import { UploadComponent } from './upload/upload.component';
 
 enableProdMode();
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    DownloadComponent,
+    HelpComponent,
+    Ijahv1Component,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(ROUTES, { useHash: true }),
+    FormsModule,
+    HttpModule,
+    FileUploadModule,
+    ChartsModule,
+    Ng2PageScrollModule.forRoot(),
+    TreeviewModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- webserver/src/app/app.routes.ts
+++ webserver/src/app/app.routes.ts
@@ -1,7 +1,19 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { DownloadComponent } from './download/download.component';
+import { HelpComponent } from './help/help.component';
+import { Ijahv1Component } from './ijahv1/ijahv1.component';
+import { UploadComponent } from './upload/upload.component';
 
 export const ROUTES: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home',  component: HomeComponent, pathMatch: 'full' },
+  { path: 'downloads', component: DownloadComponent, pathMatch: 'full' },
+  { path: 'upload', component: UploadComponent, pathMatch: 'full' },
+  { path: 'help-faq', component: HelpComponent, pathMatch: 'full' },
+  { path: 'ijahv1', component: Ijahv1Component, pathMatch: 'full' },
+  { path: 'contact', component: ContactComponent, pathMatch: 'full' },
+  { path: 'about', component: AboutComponent, pathMatch: 'full' },
 ];
