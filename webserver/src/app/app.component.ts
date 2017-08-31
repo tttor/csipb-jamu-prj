441ee042b0a0d1ba06859b522ecad38d9f7a6c5f
hzb-imasine <antares.zenithal@gmail.com>
2017-08-31 13:49:59 +0700

    modified versioning

--- webserver/src/app/app.component.ts
+++ webserver/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { WebserverConfig } from './config_webserver';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+   private version: string;
+
+   ngOnInit() {
+     this.version = WebserverConfig['version'];
+   }
 }
