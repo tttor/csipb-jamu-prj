996367241a93427df67eddd4087d4d35e71e8307
hzb-imasine <antares.zenithal@gmail.com>
2017-08-31 09:54:27 +0700

    re-added codes in ts files, tweaked help page

--- webserver/src/app/contact/contact.component.ts
+++ webserver/src/app/contact/contact.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, OnInit, Inject, ElementRef, ViewChild
+} from '@angular/core';
+
+import {
+  FormControl, FormGroup, FormBuilder, Validators
+} from '@angular/forms';
+
+import { Http } from '@angular/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -6,10 +15,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  public name;
+  public email;
+  public affiliation;
+  public msg;
+  public subject;
 
-  constructor() { }
+  // private baseAPI = 'http://ijah.apps.cs.ipb.ac.id/api/';
+  private baseAPI = 'http://localhost/ijah-api/'; // Comment this if you run online!
+  // private baseAPI = 'http://ijah.agri.web.id/api/';
 
-  ngOnInit() {
+  constructor(public route: ActivatedRoute, private http: Http) {
+    // do nothing
   }
 
+  public ngOnInit() {
+    // do nothing
+  }
+
+  public onSubmit(): void {
+    // capture data
+    let data = {name: this.name, email: this.email, affiliation: this.affiliation,
+                message: this.msg, subject: this.subject};
+    let dataStr = JSON.stringify(data);
+    // console.log(dataStr);
+
+    // Send data to DB
+    let contactAPI = this.baseAPI + 'contact.php';
+    this.http.post(contactAPI, dataStr).map((res) => res.json()).subscribe((reply) => {
+      // TODO: acknowledgement and thank you
+    });
+
+    // clear fields
+    this.name = '';
+    this.email = '';
+    this.affiliation = '';
+    this.msg = '';
+    this.subject = '';
+  }
 }
--- webserver/src/app/upload/upload.component.ts
+++ webserver/src/app/upload/upload.component.ts
@@ -4,16 +4,73 @@ import { Http, Headers } from '@angular/http';
 import { ActivatedRoute } from '@angular/router';
 import { FileUploader } from 'ng2-file-upload';
 
+const uploadURL = 'http://ijah.apps.cs.ipb.ac.id/api-node/upload';
+
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
+  public type;
+  public content1;
+  public content2;
+  public description;
+  public name;
+  public email;
+  public affiliation;
+  public publicationTitle;
+  public publicationAuthor;
+  public publicationYear;
+  public publicationJournal;
+  public publicationLink;
+  public uploader: FileUploader = new FileUploader({url: uploadURL});
+  public hasBaseDropZoneOver: boolean = false;
 
-  constructor() { }
+  // private baseAPI = 'http://ijah.apps.cs.ipb.ac.id/api/';
+  private baseAPI = 'http://localhost/ijah-api/'; // Comment this if you run online!
+  // private baseAPI = 'http://ijah.agri.web.id/api/';
 
-  ngOnInit() {
+  constructor(public route: ActivatedRoute, private http: Http) {
+  // do nothing
+ }
+
+  public ngOnInit() {
+    this.uploader.onAfterAddingFile = (file) => { file.withCredentials = false; };
   }
 
+  public onSubmit(): void {
+    // capture data
+    let data = {type: this.type, data: this.content1 + ' | ' + this.content2,
+                description: this.description, name: this.name, email: this.email,
+                affiliation: this.affiliation, publication_detail: this.publicationTitle + ', '
+                + this.publicationAuthor + ', ' + this.publicationYear + ', '
+                + this.publicationJournal + ', ' + this.publicationLink};
+    let dataStr = JSON.stringify(data);
+    // console.log(dataStr);
+
+    // Send data to DB
+    let uploadAPI = this.baseAPI + 'upload.php';
+    this.http.post(uploadAPI, dataStr).map((res) => res.json()).subscribe((reply) => {
+      // TODO: acknowledgement and thank you
+    });
+
+    // clear fields
+    this.type = '';
+    this.content1 = '';
+    this.content2 = '';
+    this.description = '';
+    this.name = '';
+    this.email = '';
+    this.affiliation = '';
+    this.publicationTitle = '';
+    this.publicationAuthor = '';
+    this.publicationYear = '';
+    this.publicationJournal = '';
+    this.publicationLink = '';
+  }
+
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
 }
